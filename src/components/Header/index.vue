c6a151dfe7876088a239ec55a62b41d947889bb3
lzy <1462247364@qq.com>
2022-01-11 16:56:13 +0800

    重新上传

---
+++ src/components/Header/index.vue
@@ -0,0 +1,176 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="nav_left">
+        <ul>
+          <li><a href="#">关于我们</a></li>
+          <li><a href="#">反馈</a></li>
+          <li><a href="#">侵权投诉</a></li>
+        </ul>
+      </div>
+      <div class="nav_right">
+        <div class="weather">
+          <span>桂林</span>
+          <img src="./images/weather.png" class="tu">
+          <span>阴 14°C</span>
+          <span> 良</span>
+        </div>
+        <img src="./images/add.png" class="add">
+        <span class="publish">发布作品</span>
+        <span class="login">登录</span>
+      </div>
+    </div>
+    <div class="container">
+      <a href="#"><h2 class="title">摘新官网</h2></a>
+      <div class="search">
+        <input type="text" placeholder="请输入要搜索的内容">
+      </div>
+      <div class="hot-search">
+        <img src="./images/hot-search.png" alt="">
+        <span>热搜：</span>
+        <span><a href="#">社科院预测世界经济</a></span>
+        <span><a href="#">英特尔发布声明</a></span>
+        <span><a href="#">北交所首只新股申购</a></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+}
+a{
+  font-size: 16px;
+  text-decoration: none;
+}
+.header{
+  width: 100%;
+  height: 45px;
+  background-color: #000;
+  color: #fff;
+  text-align: center;
+  position:fixed;
+  top: 0;
+}
+.header .nav_left{
+  float: left;
+  height: 100%;
+  display: inline-block;
+  color: #fff;
+}
+.header .nav_left li{
+  float: left;
+  margin-left: 10px;
+  padding: 0 15px;
+  list-style: none;
+  /*background-color: red;*/
+  height: 100%;
+  line-height: 45px;
+  font-size: 14px;
+  text-align: center;
+  font-size: 16px;
+}
+.header .nav_left li a{
+  font-size: 16px;
+  color: #fff;
+
+}
+.header .nav_right {
+  float: right;
+
+}
+.header .nav_right .weather{
+  position: relative;
+  top:-5px;
+  right: 120px;
+  display: inline-block;
+  /*padding-right: 200px;*/
+}
+.header .nav_right .weather .tu{
+  position: relative;
+  top:10px;
+  right: 0px;
+}
+.header .nav_right .add{
+  position: relative;
+  top: 5px;
+  right: 80px;
+  display: inline-block;
+}
+.header .nav_right .publish{
+  position: relative;
+  top: -5px;
+  right: 80px;
+}
+.header .nav_right .login{
+  position: relative;
+  top: -5px;
+  right: 30px;
+  width: 60px;
+  height: 30px;
+  border-radius: 5px;
+  text-align: center;
+  display: inline-block;
+  line-height: 30px;
+  background-color: #ff0000;
+}
+
+
+.container{
+  width: 1080px;
+  height: 500px;
+  color: #fff;
+  margin-right: auto;
+  margin-left: auto;
+  background-color: skyblue;
+  text-align: center;
+  margin-top: -15px;
+}
+.container .title{
+  position: relative;
+  top: 30%;
+  left: 3%;
+  font-size: 36px;
+  color: #fff;
+  font-family: "Microsoft YaHei", "宋体", "Segoe UI", "Lucida Grande", Helvetica, Arial, sans-serif, FreeSans, Arimo;
+}
+.container .hot-search img{
+  position: relative;
+  top: 5px;
+  left: 0px;
+}
+.container .search{
+  width: 100%;
+  position: relative;
+  top: 240px;
+  left: 0;
+}
+.container .search input{
+  width: 500px;
+  height: 40px;
+  padding-left: 5px;
+  margin-left: 10%;
+}
+.container .hot-search{
+  position: relative;
+  top: 240px;
+  left: 3%;
+}
+.container .hot-search a{
+  color: #fff;
+}
+.container .hot-search a:hover{
+  color: #ff0000;
+}
+
+
+
+</style>
---
+++ src/components/Home/index.vue
@@ -0,0 +1,492 @@
+<template>
+  <div class="container">
+    <div class="left-container" >
+      <!--            左边导航栏-->
+      <div class="nav">
+        <ul>
+          <li><a href="#">推荐</a></li>
+          <li><a href="#">热点</a></li>
+          <li><a href="#">财经</a></li>
+          <li><a href="#">科技</a></li>
+          <li><a href="#">娱乐</a></li>
+          <li><a href="#">体育</a></li>
+          <li><a href="#">时尚</a></li>
+          <li class="more"><a href="#">更多</a>
+            <div>
+              <ul>
+                <li><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">6</a></li>
+                <li><a href="#">7</a></li>
+                <li><a href="#">8</a></li>
+                <li><a href="#">9</a></li>
+                <li><a href="#">10</a></li>
+                <li><a href="#">11</a></li>
+                <li><a href="#">12</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <!--            具体内容-->
+      <div class="main">
+        <!--                每一条当作一个div-->
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+        <div class="news">
+          <h2>千年梦想照进现实</h2>
+          <div class="detail">
+            <span class="top">置顶</span>
+            <span>新华网客户端</span>
+            <span>2万评论</span>
+            <span>11小时前</span>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="right-container">
+      <div class="detail">
+        <div class="user">
+          <img src="./images/tou.jpg">
+          <span class="username">admin</span>
+          <span>> </span>
+        </div>
+        <div class="article">
+          <ul>
+            <li>
+              <span>0</span>
+              <br/>
+              <span>获赞</span>
+            </li>
+            <li>
+              <span>0</span>
+              <br/>
+              <span>粉丝</span>
+            </li>
+            <li>
+              <span>0</span>
+              <br/>
+              <span>关注</span>
+            </li>
+          </ul>
+        </div>
+        <div class="thing">
+          <ul>
+            <li>
+              <img src="./images/tou.jpg">
+              <br/>
+              <span>写文章</span>
+            </li>
+            <li>
+              <img src="./images/tou.jpg">
+              <br/>
+              <span>发头条</span>
+            </li>
+            <li>
+              <img src="./images/tou.jpg">
+              <br/>
+              <span>写问答</span>
+            </li>
+            <li>
+              <img src="./images/tou.jpg">
+              <br/>
+              <span>发视频</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="hot">
+        <div class="header">
+          <img src="./images/hot-search.png" style="width: 20px;height: 20px;">
+          <span>头条新闻</span>
+          <img src="./images/刷新.png" style="width: 15px;height: 15px; margin-left: 40px">
+          <span class="reflash">换一换</span>
+        </div>
+        <div class="hot-news">
+          <ul>
+            <li><span>&nbsp;1</span> <a href="">昨增110例本土确诊 在河南天津陕西</a></li>
+            <li><span>&nbsp;2</span> <a href="">河南昨增本土确诊87例</a></li>
+            <li><span>&nbsp;3</span> <a href="">央视主持人赵赫因癌症去世</a></li>
+            <li><span>&nbsp;4</span> <a href="">天津本轮疫情本土病例40+13</a></li>
+            <li><span>&nbsp;5</span> <a href="">哈萨克斯坦真相浮出水面：出现内鬼</a></li>
+            <li><span>&nbsp;6</span> <a href="">天津疾控：本轮疫情源头非常不清晰</a></li>
+            <li><span>&nbsp;7</span> <a href="">深圳增4例阳性感染者 含一家三口</a></li>
+            <li><span>&nbsp;8</span> <a href="">新东方：俞敏洪博客不代表公司观点</a></li>
+            <li><span>&nbsp;9</span> <a href="">江歌母亲胜诉：以法之名惩罚薄凉者</a></li>
+            <li><span>10</span> <a href="">年轻人：国潮我们高攀不起</a></li>
+          </ul>
+        </div>
+        <div class="copyright">
+          <div class="header">
+            <span>©2022</span>
+            <span>摘新团队</span>
+          </div>
+          <div class="team">
+                        <span>
+                            地&nbsp;&nbsp;址：桂林电子科技大学花江校区
+                            <br/>
+                            学&nbsp;&nbsp;院：计算机与信息安全学院
+                        </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+.container{
+  width: 1080px;
+  background-color: #1d7db1;
+  margin-left: auto;
+  margin-right: auto;
+
+}
+.container .left-container{
+  /*background-color: pink;*/
+  height: 100%;
+  width: 75%;
+  float: left;
+}
+.container .left-container .nav{
+  width: 100%;
+  /*background-color: skyblue;*/
+  display: block;
+  margin-bottom: 20px;
+  height: 50px;
+  line-height: 50px;
+  background-color: #FFFFFF;
+}
+.container .left-container li{
+  float: left;
+  /*color: #595a5a;*/
+  width: 70px;
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+  margin: 5px 15px;
+  font-size: 18px;
+  font-family: "Microsoft YaHei", "宋体", "Segoe UI", "Lucida Grande", Helvetica, Arial, sans-serif, FreeSans, Arimo;
+}
+.container .left-container li a{
+  color: #393636;
+}
+.container .left-container li a:hover{
+  color: red;
+}
+.container .left-container .more div{
+  display: none;
+  width: 280px;
+  position: absolute;
+  top: 50px;
+  left: 520px;
+  background-color: aquamarine;
+  z-index: 100;
+  border-radius: 5px;
+}
+.container .left-container .more:hover div{
+  display: block;
+}
+.container .left-container .more div li{
+  float: left;
+  background-color: green;
+  width: 50px;
+  height: 35px;
+  text-align: center;
+  line-height: 35px;
+  margin: 5px 10px;
+  font-size: 16px;
+  font-family: "Microsoft YaHei", "宋体", "Segoe UI", "Lucida Grande", Helvetica, Arial, sans-serif, FreeSans, Arimo;
+}
+.container .left-container .main{
+  width: 100%;
+  height: 80%;
+  /*background-color: #974BE0;*/
+  display: block;
+  margin-top: 50px;
+}
+.container .left-container .main .news{
+  width: 95%;
+  /*background-color: #ef3f52;*/
+  height: 60px;
+  margin-left: -280px;
+}
+.container .left-container .main .news h2{
+  font-size: 22px;
+  font-family: "Microsoft YaHei", "宋体", "Segoe UI", "Lucida Grande", Helvetica, Arial, sans-serif, FreeSans, Arimo;
+  color: #666;
+  margin-top: 20px;
+}
+.container .left-container .main .news .detail{
+  font-size: 14px;
+  margin-top: 8px;
+}
+.container .left-container .main .news .detail .top{
+  color: red;
+}
+
+
+
+
+
+
+/*右边*/
+.container .right-container{
+  float: left;
+  /*background-color: red;*/
+  width: 20%;
+  margin-top: 20px;
+
+}
+.container .right-container .detail{
+  width: 240px;
+  height: 180px;
+  background-color:#FAFAFA;
+  border-radius: 20px;
+  margin-top: 20px;
+}
+.container .right-container .user{
+  width: 120px;
+  height: 60px;
+  border-radius: 20px;
+  margin-top: 10px;
+  text-align: center;
+  line-height: 60px;
+  display: inline-block;
+  margin-left: 10px;
+}
+.container .right-container .user img{
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin-top: -10px;
+  vertical-align: middle
+}
+.container .right-container .user span{
+  font-size: 20px;
+}
+.container .right-container .article{
+  margin-top: 5px;
+  text-align: center;
+  display: block;
+}
+.container .right-container .article li{
+  float: left;
+  width: 60px;
+  height: 40px;
+  background-color:#fff;
+  margin: 0 10px;
+  border-radius: 8px;
+}
+.container .right-container .detail .thing{
+  margin-top: 60px;
+  width: 100%;
+  text-align: center;
+  background-color:#fff;
+}
+.container .right-container .thing img{
+  width: 20px;
+  height: 20px;
+}
+.container .right-container .thing li{
+  float: left;
+  width: 40px;
+  height: 40px;
+  margin: 0 10px;
+  border-radius: 8px;
+}
+.container .right-container .hot{
+  margin-top: 220px;
+}
+.container .right-container .hot .header{
+  font-size: 18px;
+  margin-bottom: 10px;
+  margin-left: 10px;
+
+}
+.container .right-container .hot .header .reflash{
+  font-size: 14px;
+
+}
+.container .right-container .hot .hot-news{
+  width: 100%
+}
+.container .right-container .hot li{
+  width: 100%;
+  height: 30px;
+  margin-left: 10px;
+}
+.container .right-container .hot li a{
+  color: black;
+  font-size: 12px;
+  margin-left: 5px;
+}
+.container .right-container .copyright{
+  margin-top: 20px;
+}
+.container .right-container .copyright .team{
+  margin-left: 10px;
+}
+
+</style>
